7a542d3abaa3d65944bb5179e01f19ec640c48fb
JrBinladin <jaduhray@gmail.com>
2024-05-27 10:57:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const btnLikes1 = document.getElementById("btnLike1")
+const countLikes1 = document.getElementById("countLikes1")
 
+function clickLike1(){
+  let totalLikes1 = parseInt(countLikes1.value) + 1
+  countLikes1.textContent = totalLikes1.toString()
+}
+
+const btnLikes2 = document.getElementById("btnLike2")
+const countLikes2 = document.getElementById("countLikes2")
+
+function clickLike2(){
+  let totalLikes2 = parseInt(countLikes2.value) + 1
+  countLikes2.textContent = totalLikes2.toString()
+}
+
+btnLike1.addEventListener("click",clickLike1)
+btnLike2.addEventListener("click",clickLike2)
